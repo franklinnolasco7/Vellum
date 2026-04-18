1e59e46c842ad7db9d43916632bf6f638dd455a6
Frank <nolascofranklin09@gmail.com>
2026-04-18 15:25:42 +0800

    fix: add explicit esbuild dependency and bump safari target

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   envPrefix: ["VITE_", "TAURI_"],
   build: {
-    target: ["es2021", "chrome105", "safari13"],
+    target: ["es2021", "chrome105", "safari15"],
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_DEBUG,
   },
